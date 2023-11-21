c0890e6dfcda5a2e382ffb91a057ceed4e1e68a7
Naiabunner <Okechukwudaniel12@gmail.com>
2023-11-21 05:58:55 +0000

    finished hero

---
+++ src/About/about.jsx
@@ -0,0 +1,29 @@
+const About = () => {
+    return ( <>
+    <section>
+        <div className="about_wrapper">
+            <div className="about_header">
+        About
+            </div>
+            <div className="about_img_txt">
+                <img src="\src\assets\Rectangle 5.jpg" alt="orange car" className="about_car"/>
+                <div className="about_article_wrapper">
+                    <article className="about_article">
+                We are a specialized team committed to <br />
+                 providing reliable car rental services. One of  <br />
+                 the advantages of renting a car from us is  <br />
+                 offering competitive and transparent prices.  <br />
+                 By providing services such as comprehensive  <br />insurance ......
+                </article>
+                <button className="read_me">
+                    Read Me
+                </button>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    </> );
+}
+ 
+export default About;
--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -1,9 +1,10 @@
+import About from "../About/about";
 import Herosection from "../homepage/hero";
-
 
 const Landingpage = () => {
     return ( <>
     <Herosection/>
+    <About/>
     </> );
 }
  
