0b9a61f7dec88463debc7349df981043007af9bf
Naiabunner <Okechukwudaniel12@gmail.com>
2023-11-28 19:28:15 +0000

    Book now pop up

--- src/Booknowpop.jsx
+++ src/Booknowpop.jsx
@@ -1,5 +1,13 @@
 const Booknow = () => {
     return (  <>
+    <div className="booknow_wrapper">
+        <div className="booknow_details">
+            <div className="car_details">
+                <img src="" alt="" />
+            
+            </div>
+        </div>
+    </div>
     </>);
 }
  
