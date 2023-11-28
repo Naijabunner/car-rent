6f1e05c74c56214aa9e373e436fb4a6d7cfeeb51
Naiabunner <Okechukwudaniel12@gmail.com>
2023-11-28 20:12:43 +0000

    Book now pop up update

--- src/Booknowpop.jsx
+++ src/Booknowpop.jsx
@@ -4,8 +4,22 @@ const Booknow = () => {
         <div className="booknow_details">
             <div className="car_details">
                 <img src="" alt="" />
-            
+            <div className="pickup_dropoff_wrapper">
+                <div className="pickup_wrapper">
+                    <h4></h4>
+                    <p></p>
+                    <aside></aside>
+                </div>
+                <div className="dropoff_wrapper">
+                    <h4></h4>
+                    <p></p>
+                    <aside></aside>
+                </div>
             </div>
+            </div>
+        </div>
+        <div className="price_summary">
+            <h4>Price Summary</h4>
         </div>
     </div>
     </>);
