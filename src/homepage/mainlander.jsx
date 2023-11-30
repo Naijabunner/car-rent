3e34c1d7cb36c422be6333215a86f3b7c3f1be0b
Naiabunner <Okechukwudaniel12@gmail.com>
2023-11-30 19:36:33 +0000

    checking img

--- src/homepage/mainlander.jsx
+++ src/homepage/mainlander.jsx
@@ -1,10 +1,10 @@
 import Herosociallinks from "../social_links_hero";
-
+import MainPicture from "/src/assets/MainPicture.jpg"
 const Mainlander = () => {
     return ( <>
     <div className="black_bg"></div>
     <Herosociallinks />
-    <img src=".\src\assets\Main Picture.jpg" alt="Car" className="hero_img"/>
+    <img src={MainPicture} alt="Car" className="hero_img"/>
     <div className="hero_text_wrapper">
         <h1> Enjoy your life with <br /> our comfortable <br /> cars.</h1>
         <p>Carent, is ready to serve the best <br /> expirience in car rental.</p>
